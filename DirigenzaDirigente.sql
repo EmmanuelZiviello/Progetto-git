58ca79c4a9c036c53abb5685cedb09a594e1cfa0
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2023-02-02 15:36:25 +0100

    Update DirigenzaDirigente.sql

--- DirigenzaDirigente.sql
+++ DirigenzaDirigente.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "Schema_Progetto".DirigenzaDirigente
-( Cod_Dirigente "Schema_Progetto"."CodiceF" NOT NULL,
-Cod_Dirigente2 "Schema_Progetto"."CodiceF" NOT NULL,
+( Cod_Dirigente "Schema_Progetto"."CodiceF" ,
+Cod_Dirigente2 "Schema_Progetto"."CodiceF" ,
 CONSTRAINT DirigenzaDirigentePK PRIMARY KEY(Cod_Dirigente,Cod_Dirigente2),
 CONSTRAINT DirigenzaDirigenteFK1 FOREIGN KEY (Cod_Dirigente) REFERENCES  "Schema_Progetto".Dirigente(CF)  ON UPDATE CASCADE ON DELETE CASCADE,
 CONSTRAINT DirigenzaDirigenteFK2 FOREIGN KEY (Cod_Dirigente2) REFERENCES  "Schema_Progetto".Dirigente(CF)  ON UPDATE CASCADE  ON DELETE CASCADE
