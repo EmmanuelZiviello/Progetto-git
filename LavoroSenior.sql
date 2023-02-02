ec8465bac75eff60f1536b761298f60f2c5377ad
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2023-02-02 14:59:04 +0100

    Update LavoroSenior.sql

--- LavoroSenior.sql
+++ LavoroSenior.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "Schema_Progetto".LavoroSenior
-( Cod_Lab "Schema_Progetto"."CodiceL" NOT NULL,
-Cod_Senior "Schema_Progetto"."CodiceF" NOT NULL,
+( Cod_Lab "Schema_Progetto"."CodiceL" ,
+Cod_Senior "Schema_Progetto"."CodiceF" ,
 CONSTRAINT LavoroSeniorPK PRIMARY KEY(Cod_Lab,Cod_Senior),
 CONSTRAINT LavoroSeniorFK1 FOREIGN KEY (Cod_Lab) REFERENCES  "Schema_Progetto".Laboratorio(Cod_Lab)  ON UPDATE CASCADE ON DELETE CASCADE,
 CONSTRAINT LavoroSeniorFK2 FOREIGN KEY (Cod_Senior) REFERENCES  "Schema_Progetto".Senior(CF)   ON UPDATE CASCADE ON DELETE CASCADE
