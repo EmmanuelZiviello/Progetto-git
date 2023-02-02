f0bcb4197264ffcec0eb57d0e0c75d9c79a21d33
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2023-02-02 14:33:23 +0100

    Update Progetto.sql

--- Progetto.sql
+++ Progetto.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "Schema_Progetto".Progetto
-( CUP "Schema_Progetto"."CodiceP" NOT NULL,
+( CUP "Schema_Progetto"."CodiceP" ,
 Cod_Senior "Schema_Progetto"."CodiceF" NOT NULL,
 Cod_Dirigente "Schema_Progetto"."CodiceF" NOT NULL, 
-Nome VARCHAR(20) NOT NULL, 
+Nome VARCHAR(20) , 
 CONSTRAINT ProPK PRIMARY KEY(CUP,Nome),
 CONSTRAINT nome_unico UNIQUE(Nome),--la chiave primaria è composta quindi senza questo vincolo sarebbe possibile avere progetti con cup diverso ma con il nome uguale ma è richiesto che ogni nome sia presente solo una volta nel database
 CONSTRAINT ProFK1 FOREIGN KEY (Cod_Senior) REFERENCES  "Schema_Progetto".Senior(CF)  ON UPDATE CASCADE ON DELETE CASCADE,
