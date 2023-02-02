f05865b195f5f3fda8023edbe876667277a89db6
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2023-02-02 14:57:33 +0100

    Update LavoroMiddle.sql

--- LavoroMiddle.sql
+++ LavoroMiddle.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "Schema_Progetto".LavoroMiddle
-( Cod_Lab "Schema_Progetto"."CodiceL" NOT NULL,
-Cod_Middle "Schema_Progetto"."CodiceF" NOT NULL,
+( Cod_Lab "Schema_Progetto"."CodiceL",
+Cod_Middle "Schema_Progetto"."CodiceF" ,
 CONSTRAINT LavoroMiddlePK PRIMARY KEY(Cod_Lab,Cod_Middle),
 CONSTRAINT LavoroMiddleFK1 FOREIGN KEY (Cod_Lab) REFERENCES  "Schema_Progetto".Laboratorio(Cod_Lab)  ON UPDATE CASCADE ON DELETE CASCADE,
 CONSTRAINT LavoroMiddleFK2 FOREIGN KEY (Cod_Middle) REFERENCES  "Schema_Progetto".Middle(CF)   ON UPDATE CASCADE ON DELETE CASCADE
