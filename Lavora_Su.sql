8f7630be383d0ccfe18c60ae875f0fc762a010a7
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2023-02-02 14:48:49 +0100

    Update Lavora_Su.sql

--- Lavora_Su.sql
+++ Lavora_Su.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "Schema_Progetto".Lavora_Su
-( CUP "Schema_Progetto"."CodiceP" NOT NULL,
-Cod_Lab "Schema_Progetto"."CodiceL" NOT NULL,
-Nome VARCHAR(20) NOT NULL, 
+( CUP "Schema_Progetto"."CodiceP",
+Cod_Lab "Schema_Progetto"."CodiceL",
+Nome VARCHAR(20) , 
 CONSTRAINT Lavora_SuPK PRIMARY KEY(CUP,Nome,Cod_Lab),
 CONSTRAINT Lavora_SuFK1 FOREIGN KEY (Cod_Lab) REFERENCES  "Schema_Progetto".Laboratorio(Cod_Lab)  ON UPDATE CASCADE ON DELETE CASCADE,
 CONSTRAINT Lavora_SuFK2 FOREIGN KEY (CUP,Nome) REFERENCES  "Schema_Progetto".Progetto(CUP,Nome)  ON UPDATE CASCADE ON DELETE CASCADE
