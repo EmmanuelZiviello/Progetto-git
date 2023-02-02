e5e141a24aa730eff93fbaed98d150a28358a542
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2023-02-02 15:00:22 +0100

    Update LavoroDirigente.sql

--- LavoroDirigente.sql
+++ LavoroDirigente.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "Schema_Progetto".LavoroDirigente
-( Cod_Lab "Schema_Progetto"."CodiceL" NOT NULL,
-Cod_Dirigente "Schema_Progetto"."CodiceF" NOT NULL,
+( Cod_Lab "Schema_Progetto"."CodiceL" ,
+Cod_Dirigente "Schema_Progetto"."CodiceF" ,
 CONSTRAINT LavoroDirigentePK PRIMARY KEY(Cod_Lab,Cod_Dirigente),
 CONSTRAINT LavoroDirigenteFK1 FOREIGN KEY (Cod_Lab) REFERENCES  "Schema_Progetto".Laboratorio(Cod_Lab)  ON UPDATE CASCADE ON DELETE CASCADE,
 CONSTRAINT LavoroDirigenteFK2 FOREIGN KEY (Cod_Dirigente) REFERENCES  "Schema_Progetto".Dirigente(CF)   ON UPDATE CASCADE ON DELETE CASCADE
