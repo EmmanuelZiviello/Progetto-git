4498642b39b3ac65115d3f5db7d6db05be5dd947
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2023-02-02 14:53:25 +0100

    Update LavoroJunior.sql

--- LavoroJunior.sql
+++ LavoroJunior.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "Schema_Progetto".LavoroJunior
-( Cod_Lab "Schema_Progetto"."CodiceL" NOT NULL,
-Cod_Junior "Schema_Progetto"."CodiceF" NOT NULL,
+( Cod_Lab "Schema_Progetto"."CodiceL" ,
+Cod_Junior "Schema_Progetto"."CodiceF" ,
 CONSTRAINT LavoroJuniorPK PRIMARY KEY(Cod_Lab,Cod_Junior),
-CONSTRAINT LavoroJuniorFK1 FOREIGN KEY (Cod_Lab) REFERENCES  "Schema_Progetto".Laboratorio(Cod_Lab)  ON UPDATE CASCADE,
-CONSTRAINT LavoroJuniorFK2 FOREIGN KEY (Cod_Junior) REFERENCES  "Schema_Progetto".Junior(CF)  ON UPDATE CASCADE
+CONSTRAINT LavoroJuniorFK1 FOREIGN KEY (Cod_Lab) REFERENCES  "Schema_Progetto".Laboratorio(Cod_Lab)  ON UPDATE CASCADE ON DELETE CASCADE,
+CONSTRAINT LavoroJuniorFK2 FOREIGN KEY (Cod_Junior) REFERENCES  "Schema_Progetto".Junior(CF)  ON UPDATE CASCADE ON DELETE CASCADE
 );
